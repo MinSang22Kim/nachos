f5d9039c01624e3b82d2b36e3593bba68d733d39
MinSang22Kim <powerminsang9901@gmail.com>
2024-04-29 00:30:02 +0900

    Proj2 과제 - Alarm 클래스 구현

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -2,6 +2,9 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
  * until a certain time.
@@ -20,6 +23,8 @@ public class Alarm {
 	    });
     }
 
+    private Vector<KThread> waitingThreadQueue = new Vector<>(); // block된 스레드들의 리스트를 만듦
+    private Vector<Long> waitingTimeQueue = new Vector<>(); // waitingQueue의 스레드들에 대응되는 대기시간 리스트
     /**
      * The timer interrupt handler. This is called by the machine's timer
      * periodically (approximately every 500 clock ticks). Causes the current
@@ -27,7 +32,26 @@ public class Alarm {
      * that should be run.
      */
     public void timerInterrupt() {
-	KThread.currentThread().yield();
+
+        long nowTime = Machine.timer().getTime(); // 현재 시간을 nowTime 변수에 저장
+        boolean interruptStatus = Machine.interrupt().disable(); // 스레드의 인터럽트 비활성화
+
+        Iterator<KThread> threadIterator = waitingThreadQueue.iterator();
+        Iterator<Long> timeIterator = waitingTimeQueue.iterator();
+
+        while (threadIterator.hasNext() && timeIterator.hasNext()) { // 다음 스레드가 있는 경우에만 반복문 진입
+
+            KThread thread = threadIterator.next();
+            long time = timeIterator.next();
+
+            if (time < nowTime) { // 대기 인터럽트의 waketime 지난 경우
+                thread.ready(); // 조건을 만족하는 경우 해당 스레드는 ready 상태로 전이됨
+                threadIterator.remove(); // ready 상태로 세팅된 스레드는 대기 큐에서 제거
+                timeIterator.remove(); // ready 상태로 세팅된 스레드의 대기 시간도 큐에서 제거
+            }
+        }
+        KThread.currentThread().yield();
+        Machine.interrupt().restore(interruptStatus);
     }
 
     /**
@@ -44,10 +68,23 @@ public class Alarm {
      *
      * @see	nachos.machine.Timer#getTime()
      */
+
     public void waitUntil(long x) {
-	// for now, cheat just to get something working (busy waiting is bad)
-	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
+	    // for now, cheat just to get something working (busy waiting is bad)
+
+        if(x<=0) return; // x Timer tick이 0 또는 음수인 경우 기다리지 않고 즉시 반환
+
+        long wakeTime = Machine.timer().getTime() + x; // 현재시간 + x = 스레드 깨어날 시간
+        boolean interruptStatus = Machine.interrupt().disable(); // 스레드의 인터럽트 비활성화
+
+        waitingThreadQueue.addElement(KThread.currentThread()); // waitingQueue에 스레드 추가
+        waitingTimeQueue.add(wakeTime); // waitingTimeQueue에 대기시간 추가
+
+        KThread.currentThread().sleep(); // waitUntil 메소드를 호출한 현재 스레드를 block시킴
+        Machine.interrupt().restore(interruptStatus); // 인터럽트 상태 복원
+
+        // 기존 코드 삭제
+        //	while (wakeTime > Machine.timer().getTime())
+        //	    KThread.yield();
     }
 }
