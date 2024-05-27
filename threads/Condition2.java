dc45fc5cda8ebec407690a023b3b9f8400448702
MinSang22Kim <powerminsang9901@gmail.com>
2024-05-27 22:47:37 +0900

    Proj2-2 과제 - Condition 변수 구현

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -1,6 +1,8 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import java.util.Vector;
+
 
 /**
  * An implementation of condition variables that disables interrupt()s for
@@ -21,7 +23,9 @@ public class Condition2 {
      *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
      */
     public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
+        this.conditionLock = conditionLock;
+
+        waitingQueue = new Vector<>();
     }
 
     /**
@@ -31,11 +35,19 @@ public class Condition2 {
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
      */
     public void sleep() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-	conditionLock.release();
+        conditionLock.release();
 
-	conditionLock.acquire();
+        boolean intStatus = Machine.interrupt().disable();
+
+        waitingQueue.add(KThread.currentThread());
+
+        KThread.currentThread().sleep(); // Let the current thread sleep
+
+        Machine.interrupt().restore(intStatus);
+
+        conditionLock.acquire();
     }
 
     /**
@@ -43,7 +55,17 @@ public class Condition2 {
      * current thread must hold the associated lock.
      */
     public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+        boolean intStatus = Machine.interrupt().disable();
+
+        if(!waitingQueue.isEmpty())
+        {
+            ((KThread)waitingQueue.get(0)).ready();
+            waitingQueue.remove(0);
+        }
+
+        Machine.interrupt().restore(intStatus);
     }
 
     /**
@@ -51,8 +73,67 @@ public class Condition2 {
      * thread must hold the associated lock.
      */
     public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+        while(!waitingQueue.isEmpty())
+            wake();
+    }
+
+    // Place Condition2 testing code in the Condition2 class.
+
+    // Example of the "interlock" pattern where two threads strictly
+    // alternate their execution with each other using a condition
+    // variable.
+
+    private static class InterlockTest {
+        private static Lock lock;
+        private static Condition2 cv;
+
+        private static class Interlocker implements Runnable {
+            public void run () {
+                lock.acquire();
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(KThread.currentThread().getName());
+                    cv.wake();   // signal
+                    cv.sleep();  // wait
+                }
+                lock.release();
+            }
+        }
+
+        public InterlockTest () {
+            lock = new Lock();
+            cv = new Condition2(lock);
+
+            KThread ping = new KThread(new Interlocker());
+            ping.setName("ping");
+            KThread pong = new KThread(new Interlocker());
+            pong.setName("pong");
+
+            ping.fork();
+            pong.fork();
+
+            // We need to wait for ping to finish, and the proper way
+            // to do so is to join on ping.  (Note that, when ping is
+            // done, pong is sleeping on the condition variable; if we
+            // were also to join on pong, we would block forever.)
+            // For this to work, join must be implemented.  If you
+            // have not implemented join yet, then comment out the
+            // call to join and instead uncomment the loop with
+            // yields; the loop has the same effect, but is a kludgy
+            // way to do it.
+            ping.join();
+            // for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
+        }
+    }
+
+    // Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
+
+    public static void selfTest() {
+        new InterlockTest();
     }
 
     private Lock conditionLock;
+    private Vector<KThread> waitingQueue;
+
 }
